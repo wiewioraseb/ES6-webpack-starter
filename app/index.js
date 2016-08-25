1ef4948835cca98337c602f86061f2f353830a53
felix <felix@volny.co>
2016-08-26 01:09:35 +0700

    added future ES feature to test babel

--- app/index.js
+++ app/index.js
@@ -1,3 +1,10 @@
-let message = 'Hello ES6'
+'use strict';
 
-document.getElementById('app').insertAdjacentHTML('afterbegin', message);
+// using rest properties - choosen at random bc it currently raises
+// an error in all browsers, but can be transpiled by Babel
+const { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+const app = document.querySelector('#app')
+if (x && y && z.a && z.b) {
+  document.querylSelector('#app').insertAdjacentHTML('afterbegin', '<h1>works.</h1>');
+}
+
